99413f854b5c613687f5b4f02fe023c9ab8fe6a7
Aaron Markey <markeyaaron@gmail.com>
2017-01-10 22:29:11 -0500

    Initial Commit

---
+++ z-lineTests/z_lineTests.swift
@@ -0,0 +1,36 @@
+//
+//  z_lineTests.swift
+//  z-lineTests
+//
+//  Created by Aaron Markey on 1/10/17.
+//  Copyright © 2017 Aaron Markey. All rights reserved.
+//
+
+import XCTest
+@testable import z_line
+
+class z_lineTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
